67fdc8afd814c4732ff6e11cddca0f694039abcd
maxplayzreal <126032950+maxplayzreal@users.noreply.github.com>
2023-08-08 14:41:23 -0400

    Add files via upload

---
+++ Particles/particletripmine.js
@@ -0,0 +1,32 @@
+particlesJS(
+    "cool-particles",
+
+    {
+        particles: {
+            number: {
+                value: 100,
+            },
+            color: {
+                value: "#c300ff"
+            },
+            move: {
+                enable: true,
+                speed: 36
+            },
+            shape: {
+                type: "image",
+                image: {
+                    src: "https://raw.githubusercontent.com/maxplayzreal/Flash-Games-SWF/main/purplestar.png",
+                }
+            },
+            size: {
+                value: 50
+            },
+            line_linked: {
+                enable: true,
+                color: "#fff",
+                width: 2.5
+            }
+        }
+    }
+);
